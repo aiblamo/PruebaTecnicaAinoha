21fa13ad59148986f09368260eabea537812adc6
aiblamo <ainoha.blasco@studiogenesis.es>
2024-05-21 09:54:32 +0200

    buscadorOk

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,15 +12,21 @@ class PageController extends Controller
      *
      * @return \Illuminate\Contracts\View\View
      */
-    public function home()
+    public function home(Request $request)
     {
-        // Obtener todos los productos desde la base de datos
-        $products = Product::all();
+        // Obtener el término de búsqueda desde la solicitud
+        $search = $request->input('search');
+
+        // Si hay una búsqueda, filtrar los productos; de lo contrario, obtener todos los productos
+        if ($search) {
+            $products = Product::where('name', 'LIKE', "%{$search}%")->get();
+        } else {
+            $products = Product::all();
+        }
 
         // Devolver la vista 'home' con los productos
-        return view('home', ['products' => $products]);
+        return view('home', ['products' => $products, 'search' => $search]);
     }
-
     /**
      * Muestra la página de un producto específico.
      *
